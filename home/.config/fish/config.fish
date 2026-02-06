c283430a32a24c7117b35beb17a626759fdb17d9
Vaibhav Shinde <vaibhavkshinde20@gmail.com>
2026-02-06 15:19:00 +0530

    feat: initial setup

---
+++ home/.config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+
+# Added by Windsurf
+fish_add_path /Users/vshinde/.codeium/windsurf/bin
+fish_add_path /Users/vshinde/.opencode/bin
