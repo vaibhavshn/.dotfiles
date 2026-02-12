4f23167d5ceb5910f96e210a95278b631ef0dd87
Vaibhav Shinde <vaibhavkshinde20@gmail.com>
2026-02-12 16:43:05 +0530

    aliases updates

--- home/common/.config/fish/conf.d/aliases.fish
+++ home/common/.config/fish/conf.d/aliases.fish
@@ -6,12 +6,43 @@ alias gg 'lazygit'
 
 # git aliases
 
-alias ga 'git add'
+# Basic operations
 alias gs 'git status'
+alias ga 'git add'
+alias gd 'git diff'
+alias gds 'git diff --staged'
+
+# Commit
 alias gc 'git commit'
+alias gac 'git add -A && git commit'
+alias gca 'git commit --amend'
+alias gcan 'git commit --amend --no-edit'
+
+# Branch
+alias gb 'git branch'
 alias gco 'git checkout'
-alias gl 'git log'
+alias gbd 'git branch -d'
+alias gbD 'git branch -D'
+
+# Remote
+alias gf 'git fetch'
 alias gp 'git pull'
 alias ggp 'git push'
 alias ggpf 'git push --force'
-alias gac 'git add -A && git commit'
+
+# History & log
+alias gl 'git log'
+
+# Merge & rebase
+alias gm 'git merge'
+alias gr 'git rebase'
+alias gri 'git rebase -i'
+
+# Stash
+alias gst 'git stash'
+alias gstp 'git stash pop'
+alias gstl 'git stash list'
+
+# Undo
+alias grh 'git reset HEAD~1'
+alias grhh 'git reset --hard HEAD~1'
