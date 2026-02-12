03a850cd11f2ac8635015afd701f58dedc4c7d2b
Vaibhav Shinde <vaibhavkshinde20@gmail.com>
2026-02-12 16:39:37 +0530

    aliases updates

--- home/common/.config/fish/conf.d/aliases.fish
+++ home/common/.config/fish/conf.d/aliases.fish
@@ -1,12 +1,17 @@
 # Aliases
 
 alias p 'pnpm'
+alias oc 'opencode'
 alias gg 'lazygit'
+
+# git aliases
+
+alias ga 'git add'
 alias gs 'git status'
+alias gc 'git commit'
 alias gco 'git checkout'
 alias gl 'git log'
 alias gp 'git pull'
 alias ggp 'git push'
 alias ggpf 'git push --force'
-alias ls 'ls --color=auto'
-alias oc 'opencode'
+alias gac 'git add -A && git commit'
